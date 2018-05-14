690c459f8fb7a131b7deb81b1383aa3148d92c3a
SheilaAbby <sheilaabigael2@gmail.com>
2018-05-14 12:35:22 +0300

    first laravel app

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\storage;
+use App\Post;
+use DB;
+class PostsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()/*a function that runs when the class is instantiated */
+    {
+        $this->middleware('auth',['except'=>['index','show']]);/*adding exceptions to the middleware-index and show are the views we want to add exceptions to*/
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$posts=Post::all();
+        //return Post::where('title','Post Two')->get();
+       // $posts=DB::select('SELECT *FROM posts');
+        //
+        //$posts= Post::orderBy('title','desc')->get();
+        //pagination
+
+       $posts= Post::orderBy('created_at','desc')->paginate(10);
+
+        
+        return view('posts.index')->with('posts',$posts);
+        
+
+       
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $this->validate($request,[
+'title'=>'required',
+'body'=>'required',
+'cover_image'=>'image|nullable|max:1999'//validadation for the image being uploaded
+       ]);
+
+       //handling file uploading
+       if($request->hasFile('cover_image')){//check image is uploaded
+$filenameWithExt=$request->file('cover_image')->getClientOriginalName();
+
+//get only filename
+$filename=pathInfo($filenameWithExt,PATHINFO_FILENAME);
+//get the ext only
+$extension=$request->file('cover_image')->getClientOriginalExtension();
+//filename to store
+$fileNameToStore=$filename.'_'.time().'.'.$extension;
+
+//finally upload image
+$path=$request->file('cover_image')->storeAs('public/cover_images',$fileNameToStore);
+       }else{
+           $fileNameToStore ='noimage.jpg';//use this default image instead if they dont upload
+       }
+       //create posts
+       $post=new Post;
+       $post->title=$request->input('title');
+       $post->body=$request->input('body');
+       $post->user_id = auth()->user()->id;
+       $post->cover_image=$fileNameToStore;
+       $post->save();
+
+       return redirect('/posts')->with('success','Post created');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post=Post::find($id);
+        return view('posts.show')->with('post',$post);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post=Post::find($id);
+
+        //checks for correct user
+        if(auth()->user()->id !==$post->user_id){
+            return redirect('/posts')->with('error','Unauthorised page!');
+        }
+        return view('posts.edit')->with('post',$post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $this->validate($request,[
+            'title'=>'required',
+            'body'=>'required',
+                   ]);
+
+                   //handling file uploading
+       if($request->hasFile('cover_image')){//check image is uploaded
+        $filenameWithExt=$request->file('cover_image')->getClientOriginalName();
+        
+        //get only filename
+        $filename=pathInfo($filenameWithExt,PATHINFO_FILENAME);
+        //get the ext only
+        $extension=$request->file('cover_image')->getClientOriginalExtension();
+        //filename to store
+        $fileNameToStore=$filename.'_'.time().'.'.$extension;
+        
+        //finally upload image
+        $path=$request->file('cover_image')->storeAs('public/cover_images',$fileNameToStore);
+               }
+            
+                   //find posts
+                   $post=Post::find($id);
+                   $post->title=$request->input('title');
+                   $post->body=$request->input('body');
+                   if($request->hasFile('cover_image')){
+                       $post->cover_image=$fileNameToStore;
+                   }
+                   $post->save();
+            
+                   return redirect('/posts')->with('success','Post Updated');
+            
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post=Post::find($id);
+
+        //checks for the correct user so that not anyone can delete posts
+        if(auth()->user()->id !==$post->user_id){
+            return redirect('/posts')->with('error','Unauthorised page!');
+        }
+        if($post->cover_image !='noimage.jpg'){
+            //delete image
+storage::delete('public/cover_images/'.$post->cover_image);
+
+        }
+        $post->delete();
+        return redirect('/posts')->with('success','Post Removed');
+    }
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+
+@extends('layouts.app')
+
+@section('content')
+       <div class="jumbotron text-center">
+               <h1>Welcome To My Laravel Blog Project</h1>
+        <p>I will be learning how to create a simple blog site using laravel</p>
+       <p> <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>   <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+       </div>
+                
+        @endsection
+  
